e3002e6c9876bb7bc54c4a748067dff39be15fc2
sukhdeep <gillsukhdeep452@gmail.com>
2022-08-17 08:10:21 +0530

    added consume function

--- websockets/basic_websocket/server.py
+++ websockets/basic_websocket/server.py
@@ -8,3 +8,8 @@ logging.basicConfig(level=logging.INFO)
 async def consumer_handler(websocket: WebSocketClientProtocol) -> None:
     async for message in websocket:
         logging.info(f"message: {message}")
+
+async def consume(hostname: str, port: int) -> None:
+    websocket_resource_url = f"ws://{hostname}:{port}"
+    async with websockets.connect(websocket_resource_url) as websocket:
+        await consumer_handler(websocket)
